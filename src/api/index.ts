170b86d7a52e1c3175a0b1c5edc96fa8f92a7c9a
Raghib Mirza <realraghibmrz@gmail.com>
2025-06-05 18:50:01 +0100

    fix files

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,8 +9,8 @@ import {
 
 // --- Configuration ---
 // Use your deployed API URL here
-const API_BASE_URL = "http://192.168.1.42:8000";
-// const API_BASE_URL = "https://safr-backend-t4t5dvi7da-nw.a.run.app";
+// const API_BASE_URL = "http://192.168.1.42:8000";
+const API_BASE_URL = "https://safr-backend-t4t5dvi7da-nw.a.run.app";
 
 // Store the logout callback
 let authLogoutCallback: (() => Promise<void>) | null = null;
